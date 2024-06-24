2ba48e9371da5028ff9a9519ed7e0faebd3e226f
saksham-s1ngh <saksham249singh@gmail.com>
2024-06-23 22:18:14 -0400

    added count_words and count_characters methods

---
+++ main.py
@@ -0,0 +1,21 @@
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        count_words(file_contents)
+        count_characters(file_contents)
+    print() 
+
+def count_words(book_contents):
+    words = book_contents.split()
+    print(f"Word count of book: {len(words)}")
+
+def count_characters(book_contents):
+    character_count = dict()
+    for character in book_contents:
+        if character in character_count.keys():
+            character_count[character] += 1
+        else:
+            character_count[character] = 1
+    print(f"Character count, by character, of book: {character_count}")
+
+main()
